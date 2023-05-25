0635aafdddaf7ce94c7024089ba3890700376a47
Kenji <31631601+kenjimaeda54@users.noreply.github.com>
2023-05-25 15:42:36 -0300

    feat: finished timer application

--- src/screens/TimerAnimation.tsx
+++ src/screens/TimerAnimation.tsx
@@ -1,3 +1,4 @@
+import { print } from "@gorhom/bottom-sheet/lib/typescript/utilities/logger";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { Dimensions, Animated, Text, TouchableOpacity, StyleSheet, View, FlatList, Vibration } from "react-native"
 import { TextInput } from "react-native-gesture-handler";
@@ -16,168 +17,170 @@ const colors = {
 };
 
 export default function TimerAnimation() {
-  const scrollX = useRef(new Animated.Value(0)).current
+  const scroolX = useRef(new Animated.Value(0)).current
+  const timerBackground = useRef(new Animated.Value(height)).current
+  const timerAnimatedInput = useRef(new Animated.Value(timers[0])).current
   const [currentDuration, setCurrentDuration] = useState(timers[0])
-  const inputRef = useRef<TextInput>(null)
-  const timerAnimationRef = useRef(new Animated.Value(height)).current;
-  const inputTimerAnimation = useRef(new Animated.Value(timers[0])).current;
-  const buttonAnimated = useRef(new Animated.Value(0)).current;
+  const buttonAnimation = useRef(new Animated.Value(0)).current
+  const refInput = useRef<TextInput>(null)
 
 
   useEffect(() => {
-    const listenInput = inputTimerAnimation.addListener(({ value }) => {
-      inputRef?.current?.setNativeProps({
+
+    //value do addListener e o valor contido no calback das animações, nesse caso e o timing 
+    const listenerInput = timerAnimatedInput.addListener(({ value }) => {
+      refInput.current?.setNativeProps({
         text: Math.ceil(value).toString()
       })
+
     })
 
     return () => {
-      textInputAnimated.removeListener(listenInput);
+      timerAnimatedInput.removeListener(listenerInput)
     }
 
   })
 
 
-  const animatedBackground = useCallback(() => {
-    inputTimerAnimation.setValue(currentDuration)
-    Animated.sequence(
-      [
-
-        Animated.timing(buttonAnimated, {
-          toValue: 1,
-          duration: 300,
-          useNativeDriver: true
-        }), // aqui vai interferi no interpolation [0,1] // por isso comecei em 1 , para que a opacidade seja 0 quando inicar a contagem, 
-
-
-        Animated.timing(timerAnimationRef, {
-          toValue: 0,
-          duration: 300,
-          useNativeDriver: true
-        }),
-
-
-        Animated.parallel([
-
-          Animated.timing(inputTimerAnimation, {
-            toValue: 0,
-            duration: currentDuration * 1000,
-            useNativeDriver: true
-          }),
-
-
-          Animated.timing(timerAnimationRef, {
-            toValue: height,
-            duration: currentDuration * 1000,
-            useNativeDriver: true
-          })
-
-        ]),
-
-        Animated.delay(400)
-      ]).start(() => {
-        Vibration.cancel()
-        Vibration.vibrate()
-        inputTimerAnimation.setValue(currentDuration)
-        Animated.timing(buttonAnimated, {
-          toValue: 0,
-          duration: 300,
-          useNativeDriver: true
-        }).start()
-
-      })
-
-  }, [currentDuration])
 
   function renderItem({ item, index }: { item: number, index: number }) {
+
     const inputRange = [
       (index - 1) * itemSize,
-      itemSize * index, // cuidado e vezes o valor do meio não pode ser menor que os outros dois nesse casso
+      itemSize * index,  // cuidado e vezes o valor do meio não pode ser menor que os outros dois nesse casso
       // porque a logica e anterior,atual e proximo se não ira acusar erro de non decrase 
       (index + 1) * itemSize
+
     ]
 
-
-    const opacity = scrollX.interpolate({
+    const opacity = scroolX.interpolate({
       inputRange,
-      outputRange: [.4, 1, .4]
+      outputRange: [.45, 1, .45]
     })
 
-    // inputRange trabalha em conjunto com outputRange ou seja nesse caso quando a tela estiver visivel sera 0 e não visivel 90de
-    const scale = scrollX.interpolate({
+    const scale = scroolX.interpolate({
       inputRange,
-      outputRange: [.70, 1, .70]
+      outputRange: [.75, 1, .75]
     })
+
 
 
     return (
       <View style={{ width: itemSize, justifyContent: "center", alignItems: "center" }}>
         <Animated.Text style={[styles.textItem, {
-          transform: [{ scale }],
           opacity,
+          transform: [{ scale }]
         }]} >{item}</Animated.Text>
       </View>
     )
   }
 
-  function handleMomentScrool(index: number) {
+  function handleMomentScroll(index: number) {
     setCurrentDuration(timers[index])
   }
 
-  const buttonOpacityAnimated = buttonAnimated.interpolate({
+
+  const pressAnimated = useCallback(() => {
+    timerAnimatedInput.setValue(currentDuration)
+    Animated.sequence([
+
+      Animated.timing(buttonAnimation, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true
+      }), // aqui vai interferi no interpolation [0,1] // por isso comecei em 1 , para que a opacidade seja 0 quando inicar a contagem, 
+
+      Animated.timing(timerBackground, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true
+      }),
+
+      Animated.parallel([
+
+        Animated.timing(timerAnimatedInput, {
+          toValue: 0,
+          duration: currentDuration * 1000,
+          useNativeDriver: true
+        }),
+
+        Animated.timing(timerBackground, {
+          toValue: height,
+          duration: currentDuration * 1000,
+          useNativeDriver: true
+        })
+
+
+      ]),
+
+      Animated.delay(400)
+
+    ]).start(() => {
+      Vibration.cancel()
+      Vibration.vibrate()
+      timerAnimatedInput.setValue(currentDuration)
+      Animated.timing(buttonAnimation, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true
+      }).start()
+
+    })
+
+  }, [currentDuration]) // não esquece de passar o duration no useCalback, conforme o valor e atualizado no onMomentumScrollEnd essa função sera chamada
+
+
+  const opacityButton = buttonAnimation.interpolate({
     inputRange: [0, 1],
-    outputRange: [1, 0]
+    outputRange: [1, 0],
   })
 
-  const buttonTranslateAnimate = buttonAnimated.interpolate({
+  const translateYButton = buttonAnimation.interpolate({
     inputRange: [0, 1],
     outputRange: [0, 200]
+
   })
 
-  const textInputAnimated = buttonAnimated.interpolate({
+  const textInputOpacity = buttonAnimation.interpolate({
     inputRange: [0, 1],
     outputRange: [0, 1]
   })
 
 
 
+
   return (
     <View style={{ flex: 1, backgroundColor: colors.black }}>
       <Animated.View
-        style={[styles.containerOverlay, {
-          transform: [{ translateY: timerAnimationRef }] // para determinar altura dinamicamente com animação usamos sempre translateY, não height
-        }]}
+        style={[styles.containerOverlay, { transform: [{ translateY: timerBackground }] }]} // para determinar altura dinamicamente com animação usamos sempre translateY, não height
       />
-      <Animated.View style={[styles.btnView, {
-        opacity: buttonOpacityAnimated,
-        transform: [{ translateY: buttonTranslateAnimate }]
-      }]}>
-        <TouchableOpacity onPress={animatedBackground}>
+      <Animated.View style={[styles.btnView, { opacity: opacityButton, transform: [{ translateY: translateYButton }] }]}>
+        <TouchableOpacity onPress={pressAnimated}>
           <View style={styles.btn} />
         </TouchableOpacity>
       </Animated.View>
       {/*a view force e para lainhar no centro*/}
       <Animated.View style={styles.forceFlatlist}>
-        {/*a view que vai ficar em cima da outra precisa estar envoldia pelo pai que forcça a flatlist*/}
-        <Animated.View style={{ position: "absolute", width: itemSize, alignSelf: "center", justifyContent: "center", alignItems: "center", opacity: textInputAnimated }}>
-          <TextInput ref={inputRef} defaultValue={currentDuration.toString()} style={styles.textItem} />
+        {/*a view que vai ficar em cima da outra precisa estar envoldia pelo pai que força a flatlist*/}
+        <Animated.View style={[styles.viewTextInput, { opacity: textInputOpacity }]}>
+          <TextInput style={styles.textItem} ref={refInput} defaultValue={currentDuration.toString()} />
         </Animated.View>
         <Animated.FlatList
           data={timers}
           horizontal
-          onScroll={Animated.event(
-            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-            { useNativeDriver: true }
-          )}
           contentContainerStyle={{
             paddingHorizontal: itemSpacing
           }}
           bounces={false}
           pagingEnabled
-          style={{ opacity: buttonOpacityAnimated }}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scroolX } } }],
+            { useNativeDriver: true }
+          )}
           showsHorizontalScrollIndicator={false}
-          onMomentumScrollEnd={e => handleMomentScrool(Math.round(e.nativeEvent.contentOffset.x / itemSize))}
+          onMomentumScrollEnd={e => handleMomentScroll(Math.round(e.nativeEvent.contentOffset.x / itemSize))}
           snapToInterval={itemSize}
+          style={{ opacity: opacityButton }}
           decelerationRate="fast"
           keyExtractor={(_, index) => `${index}`}
           renderItem={renderItem}
@@ -208,6 +211,13 @@ const styles = StyleSheet.create({
     top: height / 3,
     left: 0,
     right: 0,
+  },
+  viewTextInput: {
+    position: "absolute",
+    width: itemSize,
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
   },
   btn: {
     width: 60,
